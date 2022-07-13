fa05b540e389aac36d22c42184d7655eb23619c3
c332030 <c332030@gmail.com>
2022-07-13 14:41:56 +0800

    feat: deploy

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,11 +8,14 @@ curl_sh(){
   sh -c "curl -sL https://github.com/cc332030$1 | sh -s -- $parameter"
 }
 
-sh -c "$1"
-
 if [ "$SSH_PRIVATE_KEY" ]; then
   curl_sh /linux/raw/master/script/init-ssh/init-ssh-pre.sh
   curl_sh /linux/raw/master/script/init-ssh/init-ssh.sh
 fi
 
 curl_sh /linux/raw/master/script/deploy/deploy.sh
+
+if [ -n "$COMMAND" ]
+then
+  sh -c "${COMMAND}" || true
+fi
